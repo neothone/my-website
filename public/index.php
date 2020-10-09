a2c0062b48759609305ac50c66a3005a71bdf240
Mathieu DUMOUTIER <mdumoutier@itroom.fr>
2020-10-09 22:38:28 +0200

    Redirect to blog

--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo 'Hello World!';
+header("Location: https://blog.mathieu.dumoutier.fr/");
+
+/*
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-180229070-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-180229070-3');
+</script>
+
+ */
