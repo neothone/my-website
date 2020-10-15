ffc11f4b378cf8449b31d9036c252bae355c3504
Mathieu DUMOUTIER <mdumoutier@itroom.fr>
2020-10-15 20:36:58 +0200

    Add Google Analytics

--- public/index.php
+++ public/index.php
@@ -23,6 +23,16 @@
     <link href="css/style-dark.css" rel="stylesheet" type="text/css" id="theme-opt" />
     <link href="css/colors/skyblue.css" rel="stylesheet" id="color-opt">
     <link href="css/mathieu-dumoutier.css" rel="stylesheet" type="text/css">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180229070-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-180229070-3');
+    </script>
+
 </head>
 
 <body>
